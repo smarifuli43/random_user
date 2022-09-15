36986eee77e25cbbeb7530f05cf72254f0a02868
smarifuli43 <smarifulislami43@gmail.com>
2022-09-15 19:27:50 +0600

    done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require("cors");
 const router = require('./routes/users.route');
 const app = express()
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
